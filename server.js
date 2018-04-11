90266479b65361803900e6e557a9db51e97cf643
learnathoner <mike.tikh@gmail.com>
2018-04-11 10:18:59 -0500

    Link server to react index
    - Create build directory
    _ Add path module

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/api/hello', (req, res) => {
+app.use(express.static(__dirname + '/client/build'));
+
+app.get('/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
+});
+
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
