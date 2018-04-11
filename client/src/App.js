1b9a0ae1bc89b3d6769c69121a171f21ccd5a290
learnathoner <mike.tikh@gmail.com>
2018-04-11 10:46:29 -0500

    Change Procfilee

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    fetch('/hello')
+      .then(res => res.json())
+      .then(res => console.log(res))
+      .catch(err => console.log('server not working', err));
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Rex</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
