a9fc1c1a27a85071d84dd6aea5412a5657e20a6d
learnathoner <mike.tikh@gmail.com>
2018-04-14 11:54:36 -0500

    Add queries and connection to postgres server

--- server/index.js
+++ server/index.js
@@ -1,19 +1,23 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {
+  promiseQuery,
+  insertQuery,
+  updateQuery,
+  deleteQuery
+} = require('../database/index');
+const { FETCH_BOOKS } = require('../database/queries');
 
 // var db = require('../database');
-
 
 var app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-
-app.get('/hello', function (req, res) {
+app.get('/hello', function(req, res) {
   res.json('hey');
 });
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
 });
-
